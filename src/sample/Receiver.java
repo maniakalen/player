116d38ac06b95d73d96e71dabfa826f5067e195b
Petar Georgiev <pesho10@yahoo.com>
2017-09-26 14:15:47 +0200

    player playing

--- src/sample/Receiver.java
+++ src/sample/Receiver.java
@@ -1,16 +1,14 @@
 package sample;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+import javax.sound.sampled.*;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Receiver extends Thread
 {
-    InputStream input;
-    public static void receive()
+    private Socket clientSocket;
+    static void receive()
     {
         Receiver r = new Receiver();
         r.start();
@@ -18,30 +16,50 @@ public class Receiver extends Thread
     @Override
     public void run()
     {
-        System.out.println("Listener running");
         try {
             ServerSocket serverSocket = new ServerSocket(6666);
-            Socket clientSocket = serverSocket.accept();
-
-            input = clientSocket.getInputStream();
-            playFile(input);
+            clientSocket = serverSocket.accept();
+            playRawSound(clientSocket.getInputStream());
         } catch (Exception ex) {
-
+            ex.printStackTrace();
         }
     }
     public static void playFile(InputStream stream)
     {
         try {
 
-            if (stream.available() > 0) {
-                System.out.println("File playing");
+            //if (stream.available() > 0) {
                 AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(stream);
                 Clip clip = AudioSystem.getClip();
                 clip.open(audioInputStream);
                 clip.start();
-            }
+            //}
         } catch (Exception ex) {
             ex.printStackTrace();
+        }
+    }
+
+    private void playRawSound(InputStream input)
+    {
+        try {
+            AudioFormat af = MicRunner.getAudioFormat();
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, af);
+            SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+
+            byte[] buffer = new byte[4096];
+
+            line.open(af, 4096);
+            line.start();
+            int nRead;
+            while (clientSocket.isConnected() && (nRead = input.read(buffer, 0, buffer.length)) != -1) {
+                line.write(buffer, 0, nRead);
+                buffer = new byte[4096];
+            }
+            line.drain();
+            line.stop();
+            line.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
         }
     }
 }
